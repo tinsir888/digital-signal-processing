0939b54d9e27fbe04a18766d5d4f35f5b06305ef
tinsir888 <912658865@qq.com>
2021-12-22 13:41:14 +0800

    Update DFT.py

--- DFT.py
+++ DFT.py
@@ -1,180 +1,196 @@
-# DFT
-# DSP Lab 2
-# 1911406
-
-import math, time
 import numpy as np
+import math
+import copy
+import timeit
+import random
 import matplotlib.pyplot as plt
-#from matplotlib.font_manager import fontManager
+#import cv2
 
+def fft1(src, dst = None):
+	
+	l = len(src)
+	n = int(math.log(l,2))
+ 
+	bfsize = np.zeros((l), dtype = "complex")
+ 
+	for i in range(n + 1):
+		if i == 0:
+			for j in range(l):	
+				bfsize[j] = src[Dec2Bin_Inverse2Dec(j, n)]
+		else:
+			tmp = copy.copy(bfsize)
+			for j in range(l):
+				pos = j%(pow(2,i))
+				if pos < pow(2, i - 1):
+					bfsize[j] = tmp[j] + tmp[j + pow(2, i - 1)] * np.exp(complex(0, -2*np.pi*pos/pow(2,i)))
+					bfsize[j + pow(2, i - 1)] = tmp[j] - tmp[j + pow(2, i - 1)] * np.exp(complex(0, -2*np.pi*pos/(pow(2,i))))
+	return bfsize
+ 
+def ifft1(src):
+ 
+	for i in range(len(src)):
+		src[i] = complex(src[i].real, -src[i].imag)
+ 
+	res = fft1(src)
+ 
+	for i in range(len(res)):
+		res[i] = complex(res[i].real, -res[i].imag)
+ 
+	return res/len(res)
+ 
+def Dec2Bin_Inverse2Dec(n, m):
+	
+	b = bin(n)[2:]
+	if len(b) != m:
+		b = "0"*(m-len(b)) + b
+	b = b[::-1]
+	return int(b,2)
 
-def plot_dft(x,y,z,Fs,N,t):
-    #    plt.rcParams['font.sans-serif']=['SimHei']
-    #    plt.rcParams['axes.unicode_minus']=False
-    fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(12, 18))
-    line1, = axes[0].plot(x, 'b')  # the source signal
-    axes[0].set_xlabel(u'sample dots')
-    axes[0].set_ylabel(u'amplitude')
-    axes[0].set_title(u'the source signal')
-    line2, = axes[1].plot([Fs/N*i for i in range(int(N/2))], [abs(com_y) for com_y in y[0:int(N/2)]],'r')
-    axes[1].set_xlabel(u'frequency/Hz')
-    axes[1].set_ylabel(u'amplitude')
-    axes[1].set_title(u'the DFT signal amplitude')
-    line3, = axes[2].plot([Fs/N*i for i in range(int(N/2))],
-                          [-math.atan(com_y.imag/com_y.real) if abs(com_y)>0.1 else 0 for com_y in y[0:int(N/2)]],'r')
-    axes[2].set_xlabel(u'frequency/Hz')
-    axes[2].set_ylabel(u'phase')
-    axes[2].set_title(u'the DFT signal phase')
-    line4, = axes[3].plot(z,'b')  # the source signal
-    axes[3].set_xlabel(u'sample dots')
-    axes[3].set_ylabel(u'amplitude')
-    axes[3].set_title(u'the recovered signal by IDFT')
-    file_name = 'DFT_simulation_' + str(t) + '.png'
-    fig.savefig(file_name,dpi=500,bbox_inches='tight')
-    plt.close()
+def fft1shift(x):
+    return np.roll(x, int(len(x) / 2))
 
+def cov_in_time(a, b):
+    c = np.tile(b, 3)
+    ma = max(len(a), len(b))
+    cov = np.convolve(a, c)
+    r = cov[ma:2 * ma]
+    return r
 
-def plot_time(dft_x, my_t, sys_t):
-    l = len(dft_x)
-    plt.figure()
-    plt.title('Time Complexity Comparison')
+def get_lowpass_mask(l, k):
+    a = np.zeros_like(l, np.double)
+    c = int(len(l) / 2)
+    a[c - k:c + k] = 1
+    return a
+
+def filt_by_frequency(s, mask):
+    f = np.fft.fft(s)
+    r = f * mask
+    return np.fft.ifft(r)
+
+def filt_by_timedomain(b, mask):
+    ift = np.fft.ifft(mask)
+    return cov_in_time(b, ift)
+
+def filter():
+    l = 50
+    k = 10
+    rang = np.arange(l)
+    s = np.sin(rang)+2*np.cos(rang)
+    mask = get_lowpass_mask(s, k)
+    res_byf = filt_by_frequency(s, mask)
+    res_byt = filt_by_timedomain(s, mask)
+    fig, subs = plt.subplots(2, 2)
+    subs[0][0].plot(s)
+    subs[0][1].plot(mask)
+    subs[1][0].plot(res_byf)
+    subs[1][1].plot(res_byt)
     plt.show()
 
 
-def plot_signal(x, xn, lbx, lby):  # given coord x when plot a signal
-    plt.figure()
-    plt.xlabel(lbx)
-    plt.ylabel(lby)
-    plt.plot(x,xn)
-    plt.show()
-
-if __name__ == "__main__":
-    Fs = 8000  # sample rate
-    Nli = [100, 200, 300, 500, 700, 800]
-    dft_x = []  # coord x for my dft
-    dftshift_x = [i for i in range(-400, 400)]  # coord x for my fftshift
-    #print(dftshift_x)
-    my_dft_tli = []  # running times of my DFT
-    sys_dft_tli = []  # running times of sys DFT
-    for p in range(len(Nli)):
-        # in condition that Fs>=2*fi
-        f1 = 1000  # 1st component in signal
-        x1 = np.array([np.cos(2*math.pi*f1/Fs*n+math.pi/8) for n in range(Nli[p])])
-        f2 = 2000  # 2nd component in signal
-        x2 = np.array([np.cos(2*math.pi*f2/Fs*n-math.pi/4)*2 for n in range(Nli[p])])
-        f3 = 3000  # 3rd component in signal
-        x3 = np.array([np.cos(2*math.pi*f3/Fs*n+math.pi/2)*3 for n in range(Nli[p])])
-        f4 = 3000
-        x4 = np.array([np.cos(2*math.pi*f4/Fs*n)+0j for n in range(Nli[p])])  # even signal
-        x5 = np.array([np.sin(2*math.pi*f4/Fs*n)*1j for n in range(Nli[p])])  # odd signal
-        x = np.array([x1[i]+x2[i]+x3[i] for i in range(Nli[p])])  # source signal for prob1&2
-
-        # prob1, 2&3
-        y = []  # the result of (my)DFT
-        ts_my_dft = time.time()  # timing
-        for k in range(Nli[p]):
-            basis = [complex(math.cos(2*math.pi/Nli[p]*k*n),math.sin(2*math.pi/Nli[p]*k*n)) for n in range(Nli[p])]
-            y.append(np.dot(x,np.transpose(basis)))
-        te_my_dft = time.time()
-        
-        #test
-        #print(y)
-
-        dft_x.append(Nli[p])  # numerical magnitudes
-        my_dft_tli.append(te_my_dft-ts_my_dft)  # running time in seconds
-        # contain the basis exp(j*2*pi/N*k*n) and the projection weight
-
-        ts_sys_dft = time.time()
-        sys_y = np.fft.fft(x)  # fft func in numpy
-        te_sys_dft = time.time()
-
-        sys_dft_tli.append(te_sys_dft-ts_sys_dft)
-
-        z = []  # the result of IDFT
-        for k in range(Nli[p]):
-            basis = [complex(math.cos(2*math.pi/Nli[p]*k*n), -math.sin(2*math.pi/Nli[p]*k*n)) for n in range(Nli[p])]
-            z.append(np.dot(y, np.transpose(basis))/Nli[p])
-
-        # when input 800 points
-        # show fftshift
-        #if Nli[p] == 800:
-        if Nli[p] == 800:
-            plt.figure()
-            plt.xlim(-400,400)
-            plt.plot(range(-400,400), sys_y)
-            plt.xlim(-Nli[p]/2,Nli[p]/2)
-            plt.title('myfftshift')
-            plt.show()
-
-            # prob4
-            # linearity
-            alpha1 = 2+0j
-            alpha2 = 3+0j
-            x6 = alpha1*x4+alpha2*x5
-            # plot_signal([_ for _ in range(len(x6))], x6, 't', 'amp')  # show x6
-            fft_x4 = np.fft.fft(x4)
-            fft_x5 = np.fft.fft(x5)
-            fft_sum = alpha1*fft_x4+alpha2*fft_x5
-            fft_x6 = np.fft.fft(x6)
-
-            res4 = fft_sum-fft_x6
-            flag = True
-            for i in range(len(res4)):
-                if abs(res4[i]) >= 1+0j:
-                    flag = False
-                    break
-            if flag:
-                print('Linearity Proved')  # all approx to 0
-
-            # Parity
-            x7 = np.array([np.sin(2*math.pi*f4/Fs*n) for n in range(Nli[p])])  # odd
-            fft_1 = np.fft.fft(x7)  # shall be imag
-            fft_2 = np.fft.fft(x4)  # shall be real
-            flag = True
-            for i in range(len(fft_2)):
-                if abs(fft_2[i].imag) >= 1:
-                    flag = False
-                    break
-            # print(len(fft_2), len(fft_1))
-            for i in range(len(fft_1)):
-                if abs(fft_1[i].real) >= 1:
-                    flag = False
-                    break
-            if flag:
-                print('Parity Proved')  # all approx to 0
-            
-            print("problem5: mean filter\n\n")
-            # prob5: mean filter
-            # time domain
-            h = np.array([0.25,0.25,0.25,0.25])
-            # print(np.fft.fft(h))
-            x = np.array([0]*3+list(x4)+[0]*3)  # zero padding
-            xi = np.array([0]*3+list(x4)+[0]*3)
-            l = len(x)-3  # 0-
-            for i in range(l):
-                x[i+3] = x[i+3]*h[0]+x[i+2]*h[1]+x[i+1]*h[2]+x[i]*h[3]  # convolution
-
-            # freq domain
-            hf = [0,0,0]+[1,0,0,0]*200+[0,0,0]
-            # hfi = np.array(np.fft.ifft(hf))
-            xf = np.fft.fft(xi)*hf
-            xf = np.fft.ifft(xf)
-            # xf = np.array([0]*3+list(xf)+[0]*3)
-
-            plt.figure()
-            plt.xlim(0,800)
-            plt.plot(range(len(x)), x, label="x")
-            plt.plot(range(len(x)), xf, label="xf")
-            plt.legend()
-            plt.show()
-        
-    plt.figure()
-    plt.plot(dft_x, my_dft_tli, label="my dft")
-    plt.plot(dft_x, sys_dft_tli, label="sys fft")
-    plt.legend()
-    plt.title("Time Complexity Comparison")
-    plt.show()
+print('任务一：手工实现fft和ifft并进行比对')
+src = np.random.randint(20,size=8)
+print('自己实现的fft:')
+print(fft1(src))
+print('自己实现的ifft:')
+print(ifft1(fft1(src)))
+print('np里面的fft:')
+hhh=np.fft.fft(src)
+print(hhh)
+print('np里面的ifft:')
+print(np.fft.ifft(hhh))
+print()
 
 
 
-# 0 1 2 3 4 5 6 7 8 9 10, len=5
+print('任务二：手写傅里叶变换和快速傅里叶变换的效率比较')
+data = []
+for i in [2**x for x in range(14)]:
+	data.append((i, timeit.timeit('fft1(x)','from __main__ import fft1;import random; x=list(range(%d)); random.shuffle(x);' %i, number=10)))
+plt.figure()
+plt.scatter(*zip(*data))
+plt.xlabel('n')
+plt.ylabel('time')
+print('手写傅里叶变换数据规模和耗时的关系如下：')
+plt.show()
+
+data = []
+for i in [2**x for x in range(14)]:
+	data.append((i, timeit.timeit('np.fft.fft(x)','import numpy as np;import random; x=list(range(%d)); random.shuffle(x);' %i, number=10)))
+plt.figure()
+plt.scatter(*zip(*data))
+plt.xlabel('n')
+plt.ylabel('time')
+print('快速傅里叶变换数据规模和耗时的关系如下：')
+plt.show()
+
+data = []
+for i in [2**x for x in range(14)]:
+	data.append((i, timeit.timeit('ifft1(x)','from __main__ import ifft1;import random; x=list(range(%d)); random.shuffle(x);' %i, number=10)))
+plt.figure()
+plt.scatter(*zip(*data))
+plt.xlabel('n')
+plt.ylabel('time')
+print('手写傅里叶逆变换数据规模和耗时的关系如下：')
+plt.show()
+
+data = []
+for i in [2**x for x in range(14)]:
+	data.append((i, timeit.timeit('np.fft.ifft(x)','import numpy as np;import random; x=list(range(%d)); random.shuffle(x);' %i, number=10)))
+plt.figure()
+plt.scatter(*zip(*data))
+plt.xlabel('n')
+plt.ylabel('time')
+print('快速傅里叶逆变换数据规模和耗时的关系如下：')
+plt.show()
+print()
+#
+
+
+print('任务三')
+print('经过fft：')
+print(hhh)
+fs1=fft1shift(hhh)
+fs2=np.fft.fftshift(hhh)
+print('再经过我的fftshift：')
+print(fs1)
+print('或者经过np内置的fftshift：')
+print(fs2)
+plt.plot(hhh,"ob")
+plt.show()
+plt.plot(fs1,"ob")
+plt.show()
+plt.plot(fs2,"ob")
+plt.show()
+print()
+#
+
+'''
+print('任务四')
+print('验证线性')
+x=np.array([1,2,3,4])
+y=np.array([2,3,4,5])
+xy=np.array(x+y)
+print(x)
+print(y)
+print(xy)
+fx=np.fft.fft(x)
+fy=np.fft.fft(y)
+fxy=np.fft.fft(xy)
+print(fx)
+print(fy)
+print(fxy)
+print(np.fft.ifft(fx))
+print(np.fft.ifft(fy))
+print(np.fft.ifft(fxy))
+print('验证对称性')
+x=np.array([1,2,3,4])
+print(x)
+fx=np.fft.fft(x)
+fy=4*np.fft.ifft(x)
+print(fx)
+print(fy)
+print()
+#
+'''
+print('任务四：滤波器')
+filter()
